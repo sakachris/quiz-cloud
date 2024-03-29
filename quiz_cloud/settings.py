d81992dea0de260a50939fa994cc808289e0480e
sakachris <sakachris90@mail.com>
2024-03-29 16:56:08 +0300

    changing database from sqlite3 to mysql

--- quiz_cloud/settings.py
+++ quiz_cloud/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'quiz_cloud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'quizcloud',
+        'USER': 'quiz',
+        'PASSWORD': 'myquizcloud',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
