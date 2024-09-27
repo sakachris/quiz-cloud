c9eb847d366a539013f07e127958922924ac939e
sakachris <sakachris90@gmail.com>
2024-09-27 14:40:50 +0300

    resolving directory tree in base_dir

--- quiz_cloud/settings/base.py
+++ quiz_cloud/settings/base.py
@@ -17,7 +17,8 @@ import os
 load_dotenv()  # Load environment variables from .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
