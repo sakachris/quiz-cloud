ad3a19a1ae674f4b0d04e5f0c0ab60f3f961d017
sakachris <sakachris90@gmail.com>
2024-10-03 14:51:30 +0300

    staging environment test18

--- quiz_cloud/settings/base.py
+++ quiz_cloud/settings/base.py
@@ -239,19 +239,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
     'django.contrib.auth.backends.ModelBackend',
 ]'''
 
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'  # Convert string to boolean
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+# EMAIL_HOST = os.getenv('EMAIL_HOST')
+# EMAIL_PORT = os.getenv('EMAIL_PORT')
+# EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'  # Convert string to boolean
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'quizcloudapp@gmail.com'
-# EMAIL_HOST_PASSWORD = 'wpsosviuiocilcbq'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'quizcloudapp@gmail.com'
+EMAIL_HOST_PASSWORD = 'wpsosviuiocilcbq'
 
 PASSWORD_RESET_TIMEOUT = 14400
 SESSION_COOKIE_AGE = 60 * 60 * 8
